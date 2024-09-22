a0b613aec45afa7c30840da4756ae4d3449dd0a4
Brandon Wolf <112419054+BMWolf57@users.noreply.github.com>
2024-09-22 14:55:05 -0400

    feat: add `NFA.true` (#3)
    Co-authored-by: Brandon Wolf <bmwolf@uvm.edu>
    Co-authored-by: F. G. Dorais <fgdorais@gmail.com>

--- Automata/NFA.lean
+++ Automata/NFA.lean
@@ -1,2 +1,3 @@
 import Automata.NFA.Alt
 import Automata.NFA.Basic
+import Automata.NFA.True
---
+++ Automata/NFA/True.lean
@@ -0,0 +1,33 @@
+import Automata.NFA.Basic
+
+namespace NFA
+
+protected def true : NFA α where
+  State := Unit
+  start _ := true
+  trans _ _ _ := true
+  final _ := true
+
+@[simp] theorem true_start (s : (NFA.true (α:=α)).State) : NFA.true.start s = true := rfl
+
+@[simp] theorem true_final (s : (NFA.true (α:=α)).State) : NFA.true.final s = true := rfl
+
+@[simp] theorem true_trans (x : α) (t s : (NFA.true (α:=α)).State) : NFA.true.trans x t s = true := rfl
+
+@[simp] theorem true_run (xs : List α) (t s : (NFA.true (α:=α)).State) : NFA.true.run xs t s = true := by
+  induction xs generalizing t s with
+  | nil => rfl
+  | cons x xs ih =>
+    simp [run]
+    exists ()
+    exact ih ..
+
+@[simp] theorem true_correct (xs : List α) : NFA.true.accept xs = true := by
+  simp [accept, -exists_prop']
+  exists (), ()
+
+theorem true_sound {xs : List α} : True → NFA.true.accept xs := by simp
+
+theorem true_exact {xs : List α} : NFA.true.accept xs → True := by simp
+
+end NFA
