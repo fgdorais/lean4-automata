b6396df09754d3b9bf478ea3df0d091d7881a734
kyserd <kyserdy@gmail.com>
2024-09-22 19:15:51 -0400

    Fixed errors in theorems

--- Automata/NFA/And.lean
+++ Automata/NFA/And.lean
@@ -25,93 +25,91 @@ instance : HAnd (NFA α) (NFA α) (NFA α) := ⟨NFA.and⟩
 (m₁ &&& m₂).run xs (t₁, t₂) (s₁, s₂) = (m₁.run xs t₁ s₁ && m₂.run xs t₂ s₂) := by
   induction xs generalizing t₁ t₂ s₁ s₂ with
   | nil =>
-    dec_lift
-    constr
+    rw [Bool.eq_iff_iff]
+    simp
+    constructor
     · intro h
-      unfold run at h ⊢
-      dec_lift at h
-      injection h with h₁ h₂
-      constr
-      · dec_lift
-        exact h₁
-      · dec_lift
-        exact h₂
+      cases h
+      constructor
+      · rfl
+      · rfl
     · intro ⟨h₁, h₂⟩
-      unfold run at h₁ h₂ ⊢
-      dec_lift at h₁ h₂ ⊢
       rw [h₁, h₂]
   | cons x xs ih =>
-    unfold run
-    dec_lift
-    constr
-    · intro ⟨(u₁, u₂), ⟨htrans, hrun⟩⟩
+    simp only [run]
+    rw [Bool.eq_iff_iff]
+    simp
+    constructor
+    · intro ⟨(u₁, u₂), htrans, hrun⟩
       rw [ih] at hrun
       rw [and_trans] at htrans
-      dec_lift at hrun htrans
-      constr
+      simp at hrun htrans
+      constructor
       · exists u₁
-        constr
-        exact htrans.1
-        exact hrun.1
+        constructor
+        · exact htrans.1
+        · exact hrun.1
       · exists u₂
-        constr
-        exact htrans.2
-        exact hrun.2
-    · intro ⟨⟨u₁, ⟨htrans₁, hrun₁⟩⟩, ⟨u₂, ⟨htrans₂, hrun₂⟩⟩⟩
+        constructor
+        · exact htrans.2
+        · exact hrun.2
+    · intro ⟨⟨u₁, htrans₁, hrun₁⟩, ⟨u₂, htrans₂, hrun₂⟩⟩
       exists (u₁, u₂)
-      constr
+      constructor
       · rw [and_trans]
-        dec_lift
-        constr
+        simp
+        constructor
         · exact htrans₁
         · exact htrans₂
       · rw [ih]
-        dec_lift
-        constr
+        simp
+        constructor
         · exact hrun₁
         · exact hrun₂
 
 theorem and_correct (xs : List α) : (m₁ &&& m₂).accept xs = (m₁.accept xs && m₂.accept xs) := by
   unfold accept
-  dec_lift
-  constr
+  rw [Bool.eq_iff_iff]
+  simp
+  constructor
   · intro h
     match h with
-    |⟨⟨⟨s₁₁, s₁₂⟩,⟨s₂₁, s₂₂⟩⟩, ⟨hrun, hstart, hfinal⟩⟩ =>
+    | ⟨(s₁₁, s₁₂), (s₂₁, s₂₂), hrun, hstart, hfinal⟩  =>
+    -- |⟨⟨⟨s₁₁, s₁₂⟩,⟨s₂₁, s₂₂⟩⟩, ⟨hrun, hstart, hfinal⟩⟩ =>
       rw [and_start] at hstart
       rw [and_final] at hfinal
       rw [and_run] at hrun
-      dec_lift at hstart hfinal hrun
-      constr
-      · exists (s₁₁, s₂₁)
-        constr
+      simp at hstart hfinal hrun
+      constructor
+      · exists s₁₁, s₂₁
+        constructor
         · exact hrun.1
-        · constr
+        · constructor
           · exact hstart.1
           · exact hfinal.1
-      · exists (s₁₂, s₂₂)
-        constr
+      · exists s₁₂, s₂₂
+        constructor
         · exact hrun.2
-        · constr
+        · constructor
           · exact hstart.2
           · exact hfinal.2
-  · intro ⟨⟨⟨s₁₁, s₁₂⟩, hrun₁, hstart₁, hfinal₁⟩, ⟨⟨s₂₁, s₂₂⟩, hrun₂, hstart₂, hfinal₂⟩⟩
-    exists ((s₁₁, s₂₁), (s₁₂, s₂₂))
-    constr
+  · intro ⟨⟨s₁₁, s₁₂, hrun₁, hstart₁, hfinal₁⟩, ⟨s₂₁, s₂₂, hrun₂, hstart₂, hfinal₂⟩⟩
+    exists (s₁₁, s₂₁), (s₁₂, s₂₂)
+    constructor
     · rw [and_run]
-      dec_lift
-      constr
+      simp
+      constructor
       · exact hrun₁
       · exact hrun₂
     · rw [and_start]
-      constr
-      · dec_lift
-        constr
+      constructor
+      · simp
+        constructor
         · exact hstart₁
         · exact hstart₂
       · rw [and_final]
-        dec_lift
-        constr
+        simp
+        constructor
         · exact hfinal₁
         · exact hfinal₂
 
@@ -119,21 +117,21 @@ theorem and_correct (xs : List α) : (m₁ &&& m₂).accept xs = (m₁.accept xs
 theorem and_sound {xs : List α} : m₁.accept xs → m₂.accept xs → (m₁ &&& m₂).accept xs := by
   intro hleft hright
   rw [and_correct]
-  dec_lift
-  constr
+  simp
+  constructor
   · exact hleft
   · exact hright
 
 theorem and_exact_left {xs : List α} : (m₁ &&& m₂).accept xs → m₁.accept xs := by
   intro h
   rw [and_correct] at h
-  dec_lift at h
+  simp at h
   exact h.left
 
 theorem and_exact_right {xs : List α} : (m₁ &&& m₂).accept xs → m₂.accept xs := by
   intro h
   rw [and_correct] at h
-  dec_lift at h
+  simp at h
   exact h.right
 
 end NFA
