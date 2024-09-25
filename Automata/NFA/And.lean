a3d6c1c7a7592f0c7985959359e555b28562d841
F. G. Dorais <fgdorais@gmail.com>
2024-09-25 01:17:25 -0400

    Fix universes and implicits, golfing

--- Automata/NFA/And.lean
+++ Automata/NFA/And.lean
@@ -1,16 +1,16 @@
 import Automata.NFA.Basic
 
 namespace NFA
-universe u₁ u₂
-variable (m₁ : NFA.{u₁} α) (m₂ : NFA.{u₂} α)
 
-protected def and : NFA α where
+protected def and (m₁ m₂ : NFA α) : NFA α where
   State := m₁.State × m₂.State
   start s := m₁.start s.fst && m₂.start s.snd
   trans x t s := m₁.trans x t.fst s.fst && m₂.trans x t.snd s.snd
   final s := m₁.final s.fst && m₂.final s.snd
 
 instance : HAnd (NFA α) (NFA α) (NFA α) := ⟨NFA.and⟩
+
+variable {m₁ : NFA.{u_1} α} {m₂ : NFA.{u_2} α}
 
 @[simp] theorem and_start (s₁ : m₁.State) (s₂ : m₂.State) :
     (m₁ &&& m₂).start (s₁,s₂) = (m₁.start s₁ && m₂.start s₂) := rfl
@@ -26,82 +26,54 @@ instance : HAnd (NFA α) (NFA α) (NFA α) := ⟨NFA.and⟩
   induction xs generalizing t₁ t₂ s₁ s₂ with
   | nil =>
     rw [Bool.eq_iff_iff]
-    simp
+    simp only [run_nil, Bool.and_eq_true]
     constructor
     · intro h
       cases h
-      constructor
-      · rfl
-      · rfl
-    · intro ⟨h₁, h₂⟩
-      rw [h₁, h₂]
+      simp
+    · intro | ⟨rfl, rfl⟩ => rfl
   | cons x xs ih =>
-    simp only [run]
     rw [Bool.eq_iff_iff]
-    simp
+    simp only [run_cons, Bool.and_eq_true]
     constructor
     · intro ⟨(u₁, u₂), htrans, hrun⟩
-      rw [ih] at hrun
-      rw [and_trans] at htrans
-      simp at hrun htrans
+      simp [ih] at htrans hrun
+      cases htrans; cases hrun
       constructor
       · exists u₁
-        constructor
-        · exact htrans.1
-        · exact hrun.1
       · exists u₂
-        constructor
-        · exact htrans.2
-        · exact hrun.2
     · intro ⟨⟨u₁, htrans₁, hrun₁⟩, ⟨u₂, htrans₂, hrun₂⟩⟩
       exists (u₁, u₂)
       simp [*]
 
-@[simp] theorem and_correct (xs : List α) : (m₁ &&& m₂).accept xs = (m₁.accept xs && m₂.accept xs) := by
-  unfold accept
+@[simp] theorem and_correct (m₁ : NFA.{u_1} α) (m₂ : NFA.{u_2} α) (xs : List α) : (m₁ &&& m₂).accept xs = (m₁.accept xs && m₂.accept xs) := by
   rw [Bool.eq_iff_iff]
-  simp
+  simp only [accept_eq_true_iff, Bool.and_eq_true]
   constructor
   · intro h
     match h with
     | ⟨(s₁₁, s₁₂), (s₂₁, s₂₂), hrun, hstart, hfinal⟩  =>
-      rw [and_start] at hstart
-      rw [and_final] at hfinal
-      rw [and_run] at hrun
       simp at hstart hfinal hrun
+      cases hstart; cases hfinal; cases hrun
       constructor
       · exists s₁₁, s₂₁
-        constructor
-        · exact hrun.1
-        · constructor
-          · exact hstart.1
-          · exact hfinal.1
       · exists s₁₂, s₂₂
-        constructor
-        · exact hrun.2
-        · constructor
-          · exact hstart.2
-          · exact hfinal.2
   · intro ⟨⟨s₁₁, s₁₂, hrun₁, hstart₁, hfinal₁⟩, ⟨s₂₁, s₂₂, hrun₂, hstart₂, hfinal₂⟩⟩
     exists (s₁₁, s₂₁), (s₁₂, s₂₂)
     simp [*]
 
-
-theorem and_sound {xs : List α} : m₁.accept xs → m₂.accept xs → (m₁ &&& m₂).accept xs := by
+theorem and_sound : m₁.accept xs → m₂.accept xs → (m₁ &&& m₂).accept xs := by
   intros; simp [*]
 
-theorem and_exact_left {xs : List α} : (m₁ &&& m₂).accept xs → m₁.accept xs := by
-  intro h
+theorem and_exact_left (h : (m₁ &&& m₂).accept xs) : m₁.accept xs := by
   simp only [and_correct, Bool.and_eq_true] at h
   exact h.left
 
-theorem and_exact_right {xs : List α} : (m₁ &&& m₂).accept xs → m₂.accept xs := by
-  intro h
+theorem and_exact_right (h : (m₁ &&& m₂).accept xs) : m₂.accept xs := by
   simp only [and_correct, Bool.and_eq_true] at h
   exact h.right
 
-theorem and_exact : (m₁ &&& m₂).accept xs → m₁.accept xs ∧ m₂.accept xs := by
-  intro h
+theorem and_exact (h : (m₁ &&& m₂).accept xs) : m₁.accept xs ∧ m₂.accept xs := by
   simp only [and_correct, Bool.and_eq_true] at h
   exact h
 
