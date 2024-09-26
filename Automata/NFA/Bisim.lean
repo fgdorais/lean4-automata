81e00f8373b04e816c1af20a055c46e305ac914c
kyserd <kyserdy@gmail.com>
2024-09-26 18:29:15 -0400

    Replace `Finite` with `Find` and `dec_lift` with `simp`

--- Automata/NFA/Bisim.lean
+++ Automata/NFA/Bisim.lean
@@ -5,8 +5,8 @@ namespace NFA
 structure Bisim {α} (m₁ m₂ : NFA α) where
   rel : m₁.State → m₂.State → Bool
   start {s₁ s₂} : rel s₁ s₂ → m₁.start s₁ = m₂.start s₂
-  transLR {s₁ s₂ t₁ x} : rel s₁ s₂ → m₁.trans x s₁ t₁ → Finite.any (λ t₂ => rel t₁ t₂ && m₂.trans x s₂ t₂)
-  transRL {s₁ s₂ t₂ x} : rel s₁ s₂ → m₂.trans x s₂ t₂ → Finite.any (λ t₁ => rel t₁ t₂ && m₁.trans x s₁ t₁)
+  transLR {s₁ s₂ t₁ x} : rel s₁ s₂ → m₁.trans x s₁ t₁ → Find.any (λ t₂ => rel t₁ t₂ && m₂.trans x s₂ t₂)
+  transRL {s₁ s₂ t₂ x} : rel s₁ s₂ → m₂.trans x s₂ t₂ → Find.any (λ t₁ => rel t₁ t₂ && m₁.trans x s₁ t₁)
   final {s₁ s₂} : rel s₁ s₂ → m₁.final s₁ = m₂.final s₂
 
 namespace Bisim
@@ -16,92 +16,84 @@ protected def id : Bisim m m where
   rel s₁ s₂ := decide (s₁ = s₂)
   start {s₁ s₂} := by
     intro h
-    dec_lift at h
+    simp at h
     rw [h]
   transLR := by
     intro _ _ t _ h ht
-    dec_lift at h ⊢
-    exists t
+    simp at h ⊢
     rw [← h, ht]
-    constr
-    rfl
-    rfl
   transRL := by
     intro _ _ t _ h ht
-    dec_lift at h ⊢
-    exists t
+    simp at h ⊢
     rw [h, ht]
-    constr
-    rfl
-    rfl
   final {s₁ s₂} := by
     intro h
-    dec_lift at h
+    simp at h
     rw [h]
 
 protected def inv (a : Bisim m₁ m₂) : Bisim m₂ m₁ where
   rel s₁ s₂ := a.rel s₂ s₁
   start {s₁ s₂} := by
     intro h
-    symmetry
+    apply Eq.symm
     exact a.start h
   transLR := a.transRL
   transRL := a.transLR
   final {s₁ s₂} := by
     intro h
-    symmetry
+    apply Eq.symm
     exact a.final h
 
 protected def comp (a : Bisim m₂ m₃) (b : Bisim m₁ m₂) : Bisim m₁ m₃ where
-  rel s₁ s₃ := Finite.any (λ s₂ => b.rel s₁ s₂ && a.rel s₂ s₃)
+  rel s₁ s₃ := Find.any (λ s₂ => b.rel s₁ s₂ && a.rel s₂ s₃)
   start {s₁ s₂} := by
     intro h
-    dec_lift at h
+    simp at h
     match h with
     | ⟨s₂,⟨hb,ha⟩⟩ =>
-      transitivity (m₂.start s₂)
+      trans (m₂.start s₂)
       exact b.start hb
       exact a.start ha
   transLR := by
     intro _ _ _ _ H h₁
-    dec_lift at H h₁ ⊢
+    simp at H h₁ ⊢
     match H with
     | ⟨s₂,⟨hb,ha⟩⟩ =>
       have H := b.transLR hb h₁
-      dec_lift at H
+      simp at H
       match H with
       | ⟨t₂,⟨htb,h₂⟩⟩ =>
         have H := a.transLR ha h₂
-        dec_lift at H
+        simp at H
         match H with
         | ⟨t₃,⟨hta,h₃⟩⟩ =>
           exists t₃
-          constr
+          constructor
           exists t₂
           exact h₃
   transRL := by
     intro _ _ _ _ H h₃
-    dec_lift at H h₃ ⊢
+    simp at H h₃ ⊢
     match H with
     | ⟨s₂,⟨hb,ha⟩⟩ =>
       have H := a.transRL ha h₃
-      dec_lift at H
+      simp at H
       match H with
       | ⟨t₂,⟨hta,h₂⟩⟩ =>
         have H := b.transRL hb h₂
-        dec_lift at H
+        simp at H
         match H with
         | ⟨t₁, ⟨htb,h₁⟩⟩ =>
           exists t₁
-          constr
+          constructor
           exists t₂
           exact h₁
   final {s₁ s₂} := by
     intro h
-    dec_lift at h
+    simp at h
     match h with
     | ⟨s₂,⟨hb,ha⟩⟩ =>
-      transitivity (m₂.final s₂)
+      trans (m₂.final s₂)
       exact b.final hb
       exact a.final ha
 
