6835094f3ecc74dcb3e4a5bd6e8cf43f24054a4a
F. G. Dorais <fgdorais@gmail.com>
2024-10-06 20:29:36 -0400

    Some fixes

--- Automata/NFA.lean
+++ Automata/NFA.lean
@@ -1,5 +1,6 @@
 import Automata.NFA.Alt
 import Automata.NFA.And
 import Automata.NFA.Basic
+import Automata.NFA.Proj
 import Automata.NFA.True
 import Automata.NFA.False
--- Automata/NFA/Proj.lean
+++ Automata/NFA/Proj.lean
@@ -1,16 +1,16 @@
 import Automata.NFA.Basic
 
 namespace NFA
-variable [Find α] [Find β] (f : α → β) (m : NFA α)
+variable [Find α] [DecidableEq β] (f : α → β) (m : NFA α)
 
 def proj : NFA β where
   State := m.State
   start s := m.start s
-  trans y s t := Find.any (λ x : α => f x = y && m.trans x s t)
+  trans y s t := Find.any fun x => f x = y && m.trans x s t
   final s := m.final s
 
 @[scoped simp] theorem proj_start (s : m.State) :
-(m.proj f).start s = m.start s := rfl
+  (m.proj f).start s = m.start s := rfl
 
 @[scoped simp] theorem proj_final (s : m.State) :
 (m.proj f).final s = m.final s := rfl
@@ -21,17 +21,17 @@ def proj : NFA β where
 @[simp] theorem proj_run (xs : List α) (s t : m.State) :
 m.run xs s t = true → (m.proj f).run (xs.map f) s t = true := by
   induction xs generalizing s t with
-  | nil =>
-    simp only [NFA.run]
-    intro
-    decide
+  | nil => simp
   | cons x xs ih =>
-    simp only [NFA.run]
     intro h
     simp at h ⊢
     match h with
     | ⟨u, htrans, hrun⟩ =>
-      decide
+      exists u
+      constructor
+      · exists x
+      · apply ih
+        exact hrun
 
 @[simp] theorem proj_nur (ys : List β) (s t : m.State) :
 (m.proj f).run ys s t = true → ∃ (xs : List α), ys = xs.map f ∧ m.run xs s t = true := by
@@ -55,14 +55,11 @@ m.run xs s t = true → (m.proj f).run (xs.map f) s t = true := by
     | ⟨u, htrans, hrun⟩ =>
       match ih u t hrun with
       | ⟨xs, hfxs, hrun₁⟩ =>
-        simp only [proj] at htrans
-        simp at htrans
         match htrans with
         | ⟨x, hfx, htrans₁⟩ =>
           exists x::xs
           constructor
-          · unfold List.map
-            rw [hfxs, hfx]
+          · simp [hfxs, hfx]
           · simp
             exists u
 
@@ -71,8 +68,7 @@ m.run xs s t = true → (m.proj f).run (xs.map f) s t = true := by
   unfold accept at h
   simp at h
   match h with
-  | ⟨⟨s₁, s₂⟩, hsrun, hsfinal⟩ =>
-    unfold proj at hsfinal
+  | ⟨s₁, s₂, hsrun, hsfinal⟩ =>
     match hsfinal with
     | ⟨hsrstart, hsrfinal⟩ =>
       match proj_nur f m ys s₁ s₂ hsrun with
@@ -80,23 +76,17 @@ m.run xs s t = true → (m.proj f).run (xs.map f) s t = true := by
       exists xs
       constructor
       · rw [hxsmap]
-      · unfold accept
-        simp
-        exists (s₁, s₂)
+      · simp
+        exists s₁, s₂
 
 theorem proj_sound (xs : List α) : m.accept xs → (m.proj f).accept (xs.map f) := by
   intro h
-  unfold accept at h
   simp at h ⊢
   match h with
-  | ⟨s,hrun,hfinal⟩ =>
-    unfold accept
-    simp
-    exists s
-    rw [proj_start]
-    rw [proj_final]
+  | ⟨s, t, hrun, hfinal⟩ =>
+    exists s, t
     constructor
-    · rw [proj_run]
+    · apply proj_run
       exact hrun
     · exact hfinal
 
