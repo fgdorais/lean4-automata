91b82edd2fe8eb7fdb0c73ea39f30e68c96175fe
kyserd <kyserdy@gmail.com>
2024-10-07 16:36:02 -0400

    Style fixes

--- Automata/NFA/Bisim.lean
+++ Automata/NFA/Bisim.lean
@@ -14,85 +14,75 @@ variable (m : NFA α) {m₁ m₂ m₃ : NFA α}
 
 protected def id : Bisim m m where
   rel s₁ s₂ := decide (s₁ = s₂)
-  start {s₁ s₂} := by
-    intro h
+  start h := by
     simp at h
     rw [h]
-  transLR := by
-    intro _ _ _ _ h ht
+  transLR h ht := by
     simp at h ⊢
     rw [← h, ht]
-  transRL := by
-    intro _ _ _ _ h ht
+  transRL h ht := by
     simp at h ⊢
     rw [h, ht]
-  final {s₁ s₂} := by
-    intro h
+  final h := by
     simp at h
     rw [h]
 
 protected def inv (a : Bisim m₁ m₂) : Bisim m₂ m₁ where
   rel s₁ s₂ := a.rel s₂ s₁
-  start {s₁ s₂} := by
-    intro h
+  start h := by
     apply Eq.symm
     exact a.start h
   transLR := a.transRL
   transRL := a.transLR
-  final {s₁ s₂} := by
-    intro h
+  final h := by
     apply Eq.symm
     exact a.final h
 
 protected def comp (a : Bisim m₂ m₃) (b : Bisim m₁ m₂) : Bisim m₁ m₃ where
   rel s₁ s₃ := Find.any (λ s₂ => b.rel s₁ s₂ && a.rel s₂ s₃)
-  start {s₁ s₂} := by
-    intro h
+  start h := by
     simp at h
     match h with
-    | ⟨s₂,⟨hb,ha⟩⟩ =>
+    | ⟨s₂, hb, ha⟩ =>
       trans (m₂.start s₂)
       exact b.start hb
       exact a.start ha
-  transLR := by
-    intro _ _ _ _ H h₁
+  transLR H h₁ := by
     simp at H h₁ ⊢
     match H with
-    | ⟨s₂,⟨hb,ha⟩⟩ =>
+    | ⟨s₂, hb, ha⟩ =>
       have H := b.transLR hb h₁
       simp at H
       match H with
-      | ⟨t₂,⟨htb,h₂⟩⟩ =>
+      | ⟨t₂, htb, h₂⟩ =>
         have H := a.transLR ha h₂
         simp at H
         match H with
-        | ⟨t₃,⟨hta,h₃⟩⟩ =>
+        | ⟨t₃, hta, h₃⟩ =>
           exists t₃
           constructor
           exists t₂
           exact h₃
-  transRL := by
-    intro _ _ _ _ H h₃
+  transRL H h₃ := by
     simp at H h₃ ⊢
     match H with
-    | ⟨s₂,⟨hb,ha⟩⟩ =>
+    | ⟨s₂, hb, ha⟩ =>
       have H := a.transRL ha h₃
       simp at H
       match H with
-      | ⟨t₂,⟨hta,h₂⟩⟩ =>
+      | ⟨t₂, hta, h₂⟩ =>
         have H := b.transRL hb h₂
         simp at H
         match H with
-        | ⟨t₁, ⟨htb,h₁⟩⟩ =>
+        | ⟨t₁, htb, h₁⟩ =>
           exists t₁
           constructor
           exists t₂
           exact h₁
-  final {s₁ s₂} := by
-    intro h
+  final h := by
     simp at h
     match h with
-    | ⟨s₂,⟨hb,ha⟩⟩ =>
+    | ⟨s₂, hb, ha⟩ =>
       trans (m₂.final s₂)
       exact b.final hb
       exact a.final ha
