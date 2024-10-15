d2d66177bbdd31bf0af130fe1d64f1420b324b26
Brandon Wolf <bmwolf@uvm.edu>
2024-10-15 13:42:49 -0400

    Renamed file to Map.lean and changed all uses of proj to map

--- Automata/NFA.lean
+++ Automata/NFA.lean
@@ -1,6 +1,6 @@
 import Automata.NFA.Alt
 import Automata.NFA.And
 import Automata.NFA.Basic
-import Automata.NFA.Proj
+import Automata.NFA.Map
 import Automata.NFA.True
 import Automata.NFA.False
--- Automata/NFA/Map.lean
+++ Automata/NFA/Map.lean
@@ -3,19 +3,19 @@ import Automata.NFA.Basic
 namespace NFA
 variable [Find α] [DecidableEq β] (f : α → β) (m : NFA α)
 
-def proj : NFA β where
+def map : NFA β where
   State := m.State
   start s := m.start s
   trans y s t := Find.any fun x => f x = y && m.trans x s t
   final s := m.final s
 
-@[scoped simp] theorem proj_start : (m.proj f).start s = m.start s := rfl
+@[scoped simp] theorem map_start : (m.map f).start s = m.start s := rfl
 
-@[scoped simp] theorem proj_final : (m.proj f).final s = m.final s := rfl
+@[scoped simp] theorem map_final : (m.map f).final s = m.final s := rfl
 
-@[scoped simp] theorem proj_trans : (m.proj f).trans y s t = Find.any (λ x : α => f x = y && m.trans x s t) := rfl
+@[scoped simp] theorem map_trans : (m.map f).trans y s t = Find.any (λ x : α => f x = y && m.trans x s t) := rfl
 
-@[simp] theorem proj_run : m.run xs s t = true → (m.proj f).run (xs.map f) s t = true := by
+@[simp] theorem map_run : m.run xs s t = true → (m.map f).run (xs.map f) s t = true := by
   induction xs generalizing s t with
   | nil => simp
   | cons x xs ih =>
@@ -29,7 +29,7 @@ def proj : NFA β where
       · apply ih
         exact hrun
 
-@[simp] theorem proj_nur : (m.proj f).run ys s t = true → ∃ (xs : List α), ys = xs.map f ∧ m.run xs s t = true := by
+@[simp] theorem map_nur : (m.map f).run ys s t = true → ∃ (xs : List α), ys = xs.map f ∧ m.run xs s t = true := by
   induction ys generalizing s t with
   | nil =>
     intro h
@@ -54,14 +54,14 @@ def proj : NFA β where
           · simp
             exists u
 
-@[simp] theorem proj_exact : (m.proj f).accept ys → ∃ (xs : List α), xs.map f = ys ∧ m.accept xs := by
+@[simp] theorem map_exact : (m.map f).accept ys → ∃ (xs : List α), xs.map f = ys ∧ m.accept xs := by
   intro h
   simp at h
   match h with
   | ⟨s₁, s₂, hsrun, hsfinal⟩ =>
     match hsfinal with
     | ⟨hsrstart, hsrfinal⟩ =>
-      match proj_nur f m hsrun with
+      match map_nur f m hsrun with
       | ⟨xs, hxsmap, hxsrun⟩ =>
       exists xs
       constructor
@@ -69,14 +69,14 @@ def proj : NFA β where
       · simp
         exists s₁, s₂
 
-theorem proj_sound : m.accept xs → (m.proj f).accept (xs.map f) := by
+theorem map_sound : m.accept xs → (m.map f).accept (xs.map f) := by
   intro h
   simp at h ⊢
   match h with
   | ⟨s, t, hrun, hfinal⟩ =>
     exists s, t
     constructor
-    · apply proj_run
+    · apply map_run
       exact hrun
     · exact hfinal
 
