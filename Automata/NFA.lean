70f46a73bbdb6e40e30d4142d0662d2f7c15b8c7
Kyrill Serdyuk <kyserdy@gmail.com>
2024-10-23 19:12:45 -0400

    feat: add `NFA.bisim` (#7)
    Co-authored-by: F. G. Dorais <fgdorais@gmail.com>

--- Automata/NFA.lean
+++ Automata/NFA.lean
@@ -1,8 +1,8 @@
 import Automata.NFA.Alt
 import Automata.NFA.And
 import Automata.NFA.Basic
+import Automata.NFA.Bisim
 import Automata.NFA.Cat
-import Automata.NFA.True
 import Automata.NFA.False
 import Automata.NFA.Map
 import Automata.NFA.Reachability
