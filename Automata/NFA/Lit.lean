02f0836dac937ff7035dea357ac0ffd51f56095c
F. G. Dorais <fgdorais@gmail.com>
2024-10-27 15:41:43 -0400

    cleanup

--- Automata/NFA/Lit.lean
+++ Automata/NFA/Lit.lean
@@ -9,55 +9,36 @@ protected def lit : NFA α where
   trans x s t := !s && t && set x
   final s := s
 
-@[simp] theorem lit_start : (NFA.lit set).start s = !s := rfl
+@[simp] theorem lit_start (s) : (NFA.lit set).start s = !s := rfl
 
-@[simp] theorem lit_final : (NFA.lit set).final s = s := rfl
+@[simp] theorem lit_final (s) : (NFA.lit set).final s = s := rfl
 
-@[simp] theorem lit_trans : (NFA.lit set).trans x t s = (!t && s && set x) := rfl
+@[simp] theorem lit_trans (x t s) : (NFA.lit set).trans x t s = (!t && s && set x) := rfl
 
-@[simp] theorem lit_run : (NFA.lit set).run xs t s = match xs with | [] => s = t | [x] => !t && s && set x | _ => false := by
-  split
-  next =>
-    simp [NFA.run]
-    constructor <;> intro | hsymm => symm; assumption
-  next x =>
-    rw [Bool.eq_iff_iff]
-    simp
-  next _ h₀ h₁ =>
-    match xs with
-    | [] =>
-      exfalso
-      exact h₀ rfl
-    | [x] =>
-      exfalso
-      exact h₁ x rfl
-    | _::_::_ =>
-      rw [Bool.eq_iff_iff]
-      simp
-
-@[simp] theorem lit_correct: (NFA.lit set).accept xs = match xs with | [x] => set x | _ => false := by
+@[simp] theorem lit_run (xs t s) : (NFA.lit set).run xs t s = match xs with | [] => t = s | [x] => !t && s && set x | _ => false := by
   rw [Bool.eq_iff_iff]
-  simp
-  match xs with
-  | [] =>
-    simp
-  | [x] =>
-    simp
-  | _::_::_ =>
-    simp
+  split
+  · simp
+  · simp
+  · next _ h₀ h₁ =>
+    rw [Bool.false_eq_true, iff_false]
+    match xs with
+    | [] => simp at h₀
+    | [_] => simp at h₁
+    | _::_::_ => simp
 
-theorem lit_sound : set x = true → (NFA.lit set).accept [x] = true := by
-  intro h
-  simp
-  exact h
+@[simp] theorem lit_correct (xs) : (NFA.lit set).accept xs = match xs with | [x] => set x | _ => false := by
+  rw [Bool.eq_iff_iff, accept_eq_true_iff]
+  simp only [lit_run]
+  split <;> simp
 
-theorem lit_exact : (NFA.lit set).accept xs = true → ∃ x, set x = true ∧ xs = [x] := by
-  simp only [lit_correct]
-  intro h
+theorem lit_sound (h : set x = true) : (NFA.lit set).accept [x] = true := by
+  simp [*]
+
+theorem lit_exact (h : (NFA.lit set).accept xs = true) : ∃ x, set x = true ∧ xs = [x] := by
+  rw [lit_correct] at h
   split at h
-  next x =>
-    exists x
-  next =>
-    contradiction
+  · simp [h]
+  · contradiction
 
 end NFA
