39eba8e17a0236bf8c810f88b83dc2f7427156e2
Brandon Wolf <bmwolf@uvm.edu>
2024-11-04 18:59:29 -0500

    feat: add `NFA.trace` and `Path`

---
+++ Automata/NFA/Trace.lean
@@ -0,0 +1,114 @@
+import Automata.NFA.Basic
+import Automata.Path
+
+namespace NFA
+variable (m : NFA α)
+open Path
+
+inductive Trace : {s t : m.State} → List α → Path s t → Prop
+| protected nil {t} : Trace [] (nil t)
+| protected cons {s t u} {p : Path u t} {x xs} : m.trans x s u → Trace xs p → Trace (x :: xs) (Path.cons s p)
+
+protected theorem Trace.append : {s t u : m.State} → {p : Path u t} → {q : Path s u} → {xs ys : List α} → Trace m xs q → Trace m ys p → Trace m (xs ++ ys) (q ++ p)
+| _, _, _, _, nil _, _, _, Trace.nil, hy => hy
+| _, _, _, _, cons _ _, _::_, _, Trace.cons ht hx, hy => Trace.cons ht (Trace.append hx hy)
+
+theorem Trace.length_eq_length : {s t : m.State} → {xs : List α} → {p : Path s t} → Trace m xs p → xs.length = p.length
+| _, _, [], nil _, .nil => rfl
+| _, _, _ :: _, cons _ _, .cons _ _ => by
+  rw [List.length_cons]
+  rw [Path.length_cons]
+  rw [length_eq_length]
+  assumption
+
+theorem run_of_trace {s t : m.State} {p : Path s t} {xs : List α} : Trace m xs p → m.run xs s t := by
+  intro h
+  induction xs generalizing s t with
+  | nil =>
+    unfold run
+    simp only [decide_eq_true_eq]
+    cases h
+    rfl
+  | cons x xs ih =>
+    match h with
+    | .cons (u:=u) htrans htrace =>
+    unfold run
+    simp
+    exists u
+    constructor
+    · exact htrans
+    · exact ih htrace
+
+theorem trace_of_run {s t : m.State} {xs : List α} : m.run xs s t → ∃ (p : Path s t), Trace m xs p := by
+  intro hrun
+  induction xs generalizing s t with
+  | nil =>
+    unfold run at hrun
+    simp at hrun
+    cases hrun
+    exists nil s
+    exact Trace.nil
+  | cons x xs ih =>
+    unfold run at hrun
+    simp at hrun
+    match hrun with
+    | ⟨u, htrans, hrun⟩ =>
+      match ih hrun with
+      | ⟨p, htrace⟩ =>
+        exists cons s p
+        exact Trace.cons htrans htrace
+
+theorem trace_drop {s t : m.State} {xs : List α} {p : Path s t} (n : Nat) : Trace m xs p → Trace m  (xs.drop n) (p.drop n) := by
+  intro htrace
+  induction htrace generalizing n with
+  | nil =>
+    match n with
+    | 0 =>
+      rw [List.drop_nil]
+      rw [Path.drop_nil_zero]
+      apply Trace.nil
+    | n+1 =>
+      rw [List.drop_nil]
+      rw [Path.drop_nil_succ]
+      apply Trace.nil
+  | cons htrans htrace ih =>
+    match n with
+    | 0 =>
+      rw [List.drop_zero]
+      rw [Path.drop_zero]
+      apply Trace.cons
+      · rw [val_cons_zero]
+        exact htrans
+      · exact htrace
+    | n+1 =>
+      rw [List.drop_succ_cons]
+      rw [Path.drop_cons_succ]
+      exact ih n
+
+theorem trace_take {s t : m.State} {xs : List α} {p : Path s t} (n : Nat) : Trace m xs p → Trace m  (xs.take n) (p.take n) := by
+  intro htrace
+  induction htrace generalizing n with
+  | nil =>
+    match n with
+    | 0 =>
+      rw [List.take_nil]
+      rw [Path.take_nil_zero]
+      apply Trace.nil
+    | n+1 =>
+      rw [List.take_nil]
+      rw [Path.take_nil_succ]
+      apply Trace.nil
+  | cons htrans _ ih =>
+    match n with
+    | 0 =>
+      rw [List.take_zero]
+      rw [Path.take_cons_zero]
+      apply Trace.nil
+    | n+1 =>
+      rw [List.take_succ_cons]
+      rw [Path.take_cons_succ]
+      apply Trace.cons
+      rw [htrans]
+      exact ih n
+
+end NFA
---
+++ Automata/Path.lean
@@ -0,0 +1,163 @@
+import Batteries
+local infix:50 " ≅ " => HEq
+
+inductive Path {σ : Type _} : σ → σ → Type _
+| nil (s) : Path s s
+| cons (s) {t u} : Path t u → Path s u
+
+namespace Path
+variable {σ}
+
+scoped syntax (name := path) "⟦" term,+ "⟧"  : term
+scoped macro_rules (kind := path)
+  | `(⟦ $a ⟧)        => `(Path.nil $a)
+  | `(⟦ $a, $as,* ⟧) => `(Path.cons $a ⟦$as,*⟧)
+
+protected def append : Path s t → Path t u → Path s u
+| nil _, q => q
+| cons s p, q => cons s (Path.append p q)
+scoped instance (s t u : σ) : HAppend (Path s t) (Path t u) (Path s u) := ⟨Path.append⟩
+
+theorem append_cons_left {r s t u : σ} (p : Path s t) (q : Path t u) : cons r p ++ q = cons r (p ++ q) := rfl
+
+theorem append_left_id {s t : σ} (p : Path s t) : ⟦s⟧ ++ p = p := rfl
+
+theorem append_right_id {s t : σ} : (p : Path s t) → p ++ ⟦t⟧ = p
+| nil _ => rfl
+| cons s p => congrArg (cons s) (append_right_id p)
+
+theorem append_assoc {s t u v : σ} : (p : Path s t) → (q : Path t u) → (r : Path u v) → (p ++ q) ++ r = p ++ (q ++ r)
+| nil _, q, r => rfl
+| cons s p, q, r => congrArg (cons s) (append_assoc p q r)
+
+theorem append_cancel_left {s t u : σ} : (p : Path s t) → {q r : Path t u} → p ++ q = p ++ r → q = r
+| nil _, _, r, h => h
+| cons s p, q, r, h =>
+  have h : p ++ q = p ++ r := by injection h
+  append_cancel_left p h
+
+protected def reverse {s t : σ} : Path s t → Path t s
+| nil _ => nil _
+| cons s p => Path.append (Path.reverse p) (cons _ (nil s))
+
+theorem reverse_nil {s : σ} : ⟦s⟧.reverse = ⟦s⟧ := rfl
+
+theorem reverse_append {s t u : σ} : (p : Path s t) → (q : Path t u) → (p ++ q).reverse = q.reverse ++ p.reverse
+| nil _, q => by rw [append_left_id, reverse_nil, append_right_id]
+| @cons _ r s _ p, q => calc
+  _ = (p ++ q).reverse ++ ⟦s, r⟧ := rfl
+  _ = (q.reverse ++ p.reverse) ++ ⟦s, r⟧ := by rw [reverse_append p q]
+  _ = q.reverse ++ (p.reverse ++ ⟦s, r⟧) := by rw [append_assoc]
+
+theorem reverse_reverse {s t : σ} : (p : Path s t) → p.reverse.reverse = p
+| nil _ => rfl
+| @cons _ s t u p => calc
+  _ = (p.reverse ++ ⟦t,s⟧).reverse := rfl
+  _ = ⟦t,s⟧.reverse ++ p.reverse.reverse := by rw [reverse_append]
+  _ = ⟦t,s⟧.reverse ++ p := by rw [reverse_reverse p]
+
+theorem append_cancel_right {s t u : σ} (p : Path t u) {q r : Path s t} : q ++ p = r ++ p → q = r := by
+  intro h
+  have h : q.reverse = r.reverse := by
+    apply append_cancel_left p.reverse
+    rw [←reverse_append, ←reverse_append, h]
+  rw [←reverse_reverse q, ←reverse_reverse r, h]
+
+protected def length {s t : σ} : Path s t → Nat
+| nil _ => 0
+| cons _ p => Path.length p + 1
+
+theorem length_nil {s : σ} : (nil s).length = 0 := rfl
+
+theorem length_cons {s t u : σ} (p : Path t u) : (cons s p).length = p.length + 1 := rfl
+
+theorem length_append {s t u : σ} : (p : Path s t) →  (q : Path t u) → (p ++ q).length = p.length + q.length
+| nil _, q => by rw [append_left_id, length_nil, Nat.zero_add]
+| cons _ p, q => by rw [append_cons_left, length_cons, length_cons, Nat.add_right_comm, length_append p q]
+
+def val : Nat → {s t : σ} → Path s t → σ
+| 0, s, _, _ => s
+| _+1, _, t, nil _ => t
+| n+1, _, _, cons _ p => val n p
+
+def take : (n : Nat) → {s t : σ} → (p : Path s t) → Path s (val n p)
+| 0, _, _, _ => nil _
+| _+1, _, _, nil _ => nil _
+| n+1, _, _, cons s p => cons s (take n p)
+
+theorem take_nil (n : Nat) {s : σ} : (nil s).take n ≅ nil s :=
+  match n with | 0 => HEq.rfl | _+1 => HEq.rfl
+
+theorem take_nil_zero {s : σ} : (nil s).take 0 = nil s := rfl
+
+theorem take_nil_succ (n : Nat) {s : σ} : (nil s).take (n+1) = nil s := rfl
+
+theorem take_cons_zero {s t u : σ} {p : Path t u} : (cons s p).take 0 = nil s :=  rfl
+
+theorem take_cons_succ (n : Nat) {s t u : σ} {p : Path t u} : (cons s p).take (n+1) = cons s (p.take n) :=  rfl
+
+def drop : (n : Nat) → {s t : σ} → (p : Path s t) → Path (val n p) t
+| 0, _, _, p => p
+| _+1, _, _, nil _ => nil _
+| n+1, _, _, cons _ p => drop n p
+
+theorem drop_nil (n : Nat) {s : σ} : (nil s).drop n ≅ nil s :=
+  match n with | 0 => HEq.rfl | _+1 => HEq.rfl
+
+theorem drop_nil_zero {s : σ} : (nil s).drop 0 = nil s := rfl
+
+theorem drop_nil_succ (n : Nat) {s : σ} : (nil s).drop (n+1) = nil s := rfl
+
+theorem drop_cons_zero {s t u : σ} {p : Path t u} : (cons s p).drop 0 = cons s p :=  rfl
+
+theorem drop_cons_succ (n : Nat) {s t u : σ} {p : Path t u} : (cons s p).drop (n+1) = p.drop n :=  rfl
+
+theorem drop_zero {s t : σ} {p : Path s t} : p.drop 0 = p := rfl
+
+theorem append_take_drop : (n : Nat) → {s t : σ} → (p : Path s t) → take n p ++ drop n p = p
+| 0, _, _, _ => rfl
+| _+1, _, _, nil _ => rfl
+| n+1, _, _, cons _ p => congrArg (cons _) (append_take_drop n p)
+
+theorem val_nil {s : σ} (i : Nat) : (nil s).val i = s :=
+  match i with | 0 => rfl | _+1 => rfl
+
+theorem val_cons_zero {s t u : σ} {p : Path t u} : (cons s p).val 0 = s := rfl
+
+theorem val_cons_succ {s t u : σ} {p : Path t u} (i : Nat) : (cons s p).val (i+1) = p.val i := rfl
+
+theorem val_cons {s t u : σ} {p : Path t u} (i : Nat) : (cons s p).val i = match i with | 0 => s | i+1 => p.val i := by
+  split
+  next => rw [val_cons_zero]
+  next => rw [val_cons_succ]
+
+theorem val_take {s t : σ} {p : Path s t} {n i : Nat} : n ≤ p.length → i ≤ n → (p.take n).val i = p.val i := by
+  intro hp hi
+  induction n, i using Nat.recDiag generalizing s t p with
+  | zero_zero => rfl
+  | zero_succ => contradiction
+  | succ_zero => rfl
+  | succ_succ n i ih =>
+    match p with
+    | nil _ => rfl
+    | cons _ _ =>
+      simp only [take_cons_succ, val_cons_succ]
+      apply ih
+      · exact Nat.le_of_succ_le_succ hp
+      · exact Nat.le_of_succ_le_succ hi
+
+theorem val_drop {s t : σ} {p : Path s t} {n i : Nat} : n ≤ p.length → i ≥ n → (p.drop n).val i = p.val (i+n) := by
+  intro hp hi
+  induction n generalizing s t p with
+  | zero => rfl
+  | succ n ih =>
+    rw [Nat.add_succ i]
+    match p with
+    | nil _ => contradiction
+    | cons _ _ =>
+      simp only [drop_cons_succ, val_cons_succ]
+      apply ih
+      · exact Nat.le_of_succ_le_succ hp
+      · omega
+
+end Path
