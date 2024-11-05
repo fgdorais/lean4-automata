8106c107cfe1fc09bade2a570d11dfe1e3a14471
kyserd <kyserdy@gmail.com>
2024-11-04 19:01:09 -0500

    Add abbrevation for `lg2`

--- Automata/Basic.lean
+++ Automata/Basic.lean
@@ -6,3 +6,8 @@ structure StateType.{u} where
   State : Type u
   [instEnum : Fin.Enum State]
 attribute [instance] StateType.instEnum
+
+
+namespace Nat
+abbrev lg2 (n : Nat) := if n = 0 then 0 else n.log2 + 1
+end Nat
