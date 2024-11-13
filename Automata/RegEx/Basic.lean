4c6f2c1028fe3712357496f46a6e10c78e83b13b
F. G. Dorais <fgdorais@gmail.com>
2024-11-12 20:58:27 -0500

    Cleanup defs

--- Automata/RegEx/Basic.lean
+++ Automata/RegEx/Basic.lean
@@ -1,29 +1,33 @@
 
 /-- Regular expression data type -/
 inductive RegEx (α : Type _) : Type _
-| empty : RegEx α
-| nil : RegEx α
-| lit : (α → Bool) → RegEx α
-| alt : RegEx α → RegEx α → RegEx α
-| cat : RegEx α → RegEx α → RegEx α
-| star : RegEx α → RegEx α
-deriving Inhabited
+  | empty : RegEx α
+  | nil : RegEx α
+  | lit : (α → Bool) → RegEx α
+  | alt : RegEx α → RegEx α → RegEx α
+  | cat : RegEx α → RegEx α → RegEx α
+  | star : RegEx α → RegEx α
+  deriving Inhabited
 
-def RegEx.toString : RegEx α → String
-| empty => "()"
-| nil => "ε"
-| lit _ => "[lit]"
-| alt a b => toString a ++ " | " ++ toString b
-| cat a b =>
-  match a with
-  | alt _ _ => "(" ++ toString a ++ " " ++ toString b ++ ")"
-  | _ => toString a ++ " " ++ toString b
-| star a =>
-  match a with
-  | alt _ _ => "(" ++ toString a ++ ")*"
-  | cat _ _ => "(" ++ toString a ++ ")*"
-  | _ => toString a ++ "*"
-instance : ToString (RegEx α) := ⟨RegEx.toString⟩
+protected class RegEx.SetRepr (α : Type _) where
+  toString (s : α → Bool) : String
+  ofString : String → α → Bool
+
+def RegEx.toString [RegEx.SetRepr α] : RegEx α → String
+  | empty => "[]"
+  | nil => ""
+  | lit s => "[" ++ SetRepr.toString s ++ "]"
+  | alt a b => toString a ++ " | " ++ toString b
+  | cat a b =>
+    match a with
+    | alt _ _ => "(" ++ toString a ++ " " ++ toString b ++ ")"
+    | _ => toString a ++ " " ++ toString b
+  | star a =>
+    match a with
+    | alt _ _ => "(" ++ toString a ++ ")*"
+    | cat _ _ => "(" ++ toString a ++ ")*"
+    | _ => toString a ++ "*"
+  instance [RegEx.SetRepr α] : ToString (RegEx α) := ⟨RegEx.toString⟩
 
 /- Regular expression syntax category -/
 declare_syntax_cat regex
@@ -32,16 +36,14 @@ syntax regex:41 "|" regex:40 : regex
 syntax regex regex:arg : regex
 syntax regex:max "*" : regex
 syntax "(" regex ")" : regex
-syntax "[" term,* "]" : regex
-syntax "{" term "}" : regex
+syntax "[" str "]" : regex
 
 /- Regular expression syntax parser -/
 syntax "toRegEx%" regex : term
 macro_rules
-| `(toRegEx% .) => `(RegEx.lit fun _ => true)
-| `(toRegEx% $a | $b) => `(RegEx.alt (toRegEx% $a) (toRegEx% $b))
-| `(toRegEx% $a $b) => `(RegEx.cat (toRegEx% $a) (toRegEx% $b))
-| `(toRegEx% $a *) => `(RegEx.star (toRegEx% $a))
-| `(toRegEx% ( $a )) => `(toRegEx% $a)
-| `(toRegEx% [ $t,* ]) => `(RegEx.lit fun x => x ∈ [$t,*])
-| `(toRegEx% { $t }) => `(RegEx.lit $t)
+  | `(toRegEx% .) => `(RegEx.lit fun _ => true)
+  | `(toRegEx% $a | $b) => `(RegEx.alt (toRegEx% $a) (toRegEx% $b))
+  | `(toRegEx% $a $b) => `(RegEx.cat (toRegEx% $a) (toRegEx% $b))
+  | `(toRegEx% $a *) => `(RegEx.star (toRegEx% $a))
+  | `(toRegEx% ( $a )) => `(toRegEx% $a)
+  | `(toRegEx% [ $s ]) => `(RegEx.lit (RegEx.SetRepr.ofString $s))
