b4124a226e9d8430d022931e4cb6cec07c97b634
F. G. Dorais <fgdorais@gmail.com>
2025-02-21 03:48:21 -0500

    feat: NFA.Matrix type

--- Automata/Basic.lean
+++ Automata/Basic.lean
@@ -16,3 +16,23 @@ theorem Nat.lt_2_pow_lg2 (x : Nat) : x < 2 ^ x.lg2 := by
   split
   · simp [*]
   · exact Nat.lt_log2_self
+
+def BitVec.dot (a b : BitVec size) : Bool := (a &&& b) != 0
+
+abbrev BitMat (rows cols) := Vector (BitVec cols) rows
+
+namespace BitMat
+
+def row (a : BitMat r c) (i) (h : i < r := by get_elem_tactic) : BitVec c := a[i]
+
+def col (a : BitMat r c) (j) (h : j < c := by get_elem_tactic) : BitVec r :=
+  Fin.foldr r (fun i t => t.shiftConcat a[i][j]) 0
+
+def bvMul (a : BitMat r c) (x : BitVec c) : BitVec r :=
+  Fin.foldr r (fun i t => t.shiftConcat (a[i].dot x)) 0
+
+def combo (a : BitMat r c) (x : BitVec r) : BitVec c :=
+  Fin.foldr r (fun i t => bif x[i] then t ||| a[i] else t) 0
+
+protected def mul (a : BitMat r m) (b  : BitMat m c) : BitMat r c :=
+  .ofFn fun i => combo b a[i]
--- Automata/NFA/Matrix.lean
+++ Automata/NFA/Matrix.lean
@@ -1,28 +1,27 @@
 import Automata.Basic
 
-structure MFA (α : Type _) where
+namespace NFA
+
+@[ext] structure Matrix (α : Type _) where
   size : Nat
-  start : Vector size Bool
-  final : Vector size Bool
-  trans : α → Matrix size size Bool
+  start : BitVec size
+  final : BitVec size
+  trans (x : α) : BitMat size size
 
-namespace MFA
-variable {α} (m : MFA α)
-
-protected theorem eq : {m₁ m₂ : MFA α} → m₁.size = m₂.size → m₁.start ≅ m₂.start → m₁.final ≅ m₂.final → m₁.trans ≅ m₂.trans → m₁ = m₂
-| ⟨_,_,_,_⟩, ⟨_,_,_,_⟩, rfl, HEq.rfl, HEq.rfl, HEq.rfl => rfl
+namespace Matrix
+variable (m : Matrix α)
 
 abbrev State := Fin m.size
 
-def run : List α → (start : Vector m.size Bool := m.start) → Vector m.size Bool
+def run : List α → (start : BitVec m.size := m.start) → BitVec m.size
 | [], start => start
-| x::xs, start => run xs (start := (m.trans x).mulVecBool start)
+| x::xs, start => run xs (start := (m.trans x).combo start)
 
-theorem run_nil {start : Vector m.size Bool} : m.run [] start = start := rfl
+theorem run_nil {start : BitVec m.size} : m.run [] start = start := rfl
 
-theorem run_cons (x : α) (xs : List α) {start : Vector m.size Bool} : m.run (x :: xs) start = m.run xs ((m.trans x).mulVecBool start) := rfl
+theorem run_cons (x : α) (xs : List α) {start : BitVec m.size} : m.run (x :: xs) start = m.run xs ((m.trans x).combo start) := rfl
 
-theorem run_append (xs ys : List α) {start : Vector m.size Bool} : m.run (xs ++ ys) start = m.run ys (m.run xs start) := by
+theorem run_append (xs ys : List α) {start : BitVec m.size} : m.run (xs ++ ys) start = m.run ys (m.run xs start) := by
   induction xs generalizing start with
   | nil => rfl
   | cons x xs ih =>
@@ -31,8 +30,6 @@ theorem run_append (xs ys : List α) {start : Vector m.size Bool} : m.run (xs ++
     rw [run_cons]
     rw [ih]
 
-abbrev accept (xs : List α) (start : Vector m.size Bool := m.start) : Bool := m.final.dotBool (m.run xs start)
+abbrev accept (xs : List α) (start : BitVec m.size := m.start) : Bool := m.final.dot (m.run xs start)
 
-theorem accept_def (xs : List α) {start : Vector m.size Bool} : m.accept xs start = m.final.dotBool (m.run xs start) := rfl
-
-end MFA
+theorem accept_def (xs : List α) {start : BitVec m.size} : m.accept xs start = m.final.dot (m.run xs start) := rfl
