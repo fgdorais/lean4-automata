eeab724196b283a0c3c868702b2e4f648f509cca
Brandon Wolf <bmwolf@uvm.edu>
2025-02-24 18:17:38 -0500

    working on toNFA

---
+++ Automata/NFA/ToNFA.lean
@@ -0,0 +1,80 @@
+import Automata.NFA.Basic
+import Automata.NFA.Matrix
+
+namespace NFA.Matrix
+variable (m : Matrix α)
+open NFA
+
+abbrev toNFA : NFA α where
+  State := Fin m.size
+  start i := m.start[i]
+  final i := m.final[i]
+  trans x i j := (m.trans x)[j][i]
+
+theorem toNFA_run {start : BitVec m.size} :
+  (Find.any fun s => start[s] && m.toNFA.run xs s t) = (m.run xs start)[t] := by
+  simp only [Fin.getElem_fin]
+  induction xs generalizing start t with
+  | nil =>
+    rw [Bool.eq_iff_iff]
+    constructor
+    · simp only [Find.any_iff_exists, Bool.and_eq_true, NFA.run_nil, exists_eq_right]
+      rw[Matrix.run_nil]
+      intro
+      assumption
+    · intro hrun
+      rw [Matrix.run_nil] at hrun
+      simp only [Find.any_iff_exists, Bool.and_eq_true, NFA.run_nil, exists_eq_right]
+      exact hrun
+  | cons x xs ih =>
+    constructor
+    · intro ⟨s, hstart, hrun⟩
+      rw [NFA.run_cons] at hrun
+      match hrun with
+      | ⟨u, htrans, hrun⟩ =>
+        rw [MFA.run_cons]
+        unfold Matrix.mulVecBool
+        rw [←ih]
+        exists u
+        constructor
+        · rw [Vector.get_ofFn]
+          simp
+          exists s
+        · exact hrun
+    · intro hrun
+      rw [MFA.run_cons] at hrun
+      unfold Matrix.mulVecBool at hrun
+      rw [←ih] at hrun
+      match hrun with
+      | ⟨i, htrans, hrun⟩ =>
+        rw [Vector.get_ofFn] at htrans
+        simp at htrans
+        match htrans with
+        | ⟨j, htrans, hstart⟩ =>
+          exists j
+          constructor
+          · exact hstart
+          · rw [NFA.run_cons]
+            exists i
+
+theorem toNFA_correct : m.toNFA.accept xs = m.accept xs := by
+  unfold NFA.accept Matrix.accept
+  simp only [Fin.getElem_fin]
+  rw [Bool.eq_iff_iff]
+  simp only [Find.any_iff_exists, Bool.and_eq_true, Prod.exists]
+  constructor
+  · intro ⟨s, t, hrun, hstart, hfinal⟩
+    exists t
+    constructor
+    · exact hfinal
+    · rw [←toNFA_run]
+      simp
+      exists s
+  · intro ⟨t, hfinal, hrun⟩
+    rw [←toNFA_run] at hrun
+    simp at hrun
+    match hrun with
+    | ⟨s, hstart, hrun⟩ =>
+      exists (s,t)
+
+end NFA.Matrix
